6e6a386dc1238571c74147091ddaeb6bc16623c8
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-02-02 10:27:30 -0800

    yaaaas check in isworking 🧚🏾‍♂️

--- checkIn/checkIn.js
+++ checkIn/checkIn.js
@@ -4,6 +4,16 @@ import { sleep, mood, energy, mbs } from '../data.js';
 
 const sleepH1 = document.getElementById('sleep-id');
 const sleepForm = document.getElementById('sleep-form');
+const sleepSection = document.getElementById('sleep-section');
+const moodSection = document.getElementById('mood-section');
+const energySection = document.getElementById('energy-section');
+const mbsSection = document.getElementById('mbs-section');
+
+
+sleepSection.style.display = 'flex';
+moodSection.style.display = 'none';
+energySection.style.display = 'none';
+mbsSection.style.display = 'none';
 
 
 sleepH1.textContent = sleep.question;
@@ -66,3 +76,199 @@ for (let item of mbs.choices) {
 
     mbsForm.append(choices);
 }
+
+// sleep event listners //
+
+const sleepButton = document.createElement('button');
+sleepButton.textContent = 'Submit!';
+sleepForm.appendChild(sleepButton);
+
+
+sleepForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // const formData = new FormData(form);
+
+    
+    // const selectionId = formData.get('choices');
+    // const choice = findById(quest.choices, selectionId);
+    // const user = JSON.parse(localStorage.getItem('USER'));
+
+    // questImage.src = `../assets/${choice.render}`;
+
+    
+    // resultsSpan.textContent = choice.result;
+    // // use the selectionId to set the property dynamically //
+    // user.completed[questId] = true;
+    
+    
+    
+    
+    // if (choice.house) {
+    //     user.house = choice.house;
+    // }   
+    // if (choice.pet) {
+    //     user.pet = choice.pet;
+    // }   
+   
+    // if (choice.wand) {
+    //     user.wand = choice.wand;
+    // }
+    
+    // localStorage.setItem('USER', JSON.stringify(user));
+    
+    sleepSection.style.display = 'none';
+    moodSection.style.display = 'flex';
+
+    
+    
+});
+
+// mood event listners //
+
+const moodButton = document.createElement('button');
+moodButton.textContent = 'Submit!';
+moodForm.appendChild(moodButton);
+
+// event listners //
+moodForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // const formData = new FormData(form);
+
+    
+    // const selectionId = formData.get('choices');
+    // const choice = findById(quest.choices, selectionId);
+    // const user = JSON.parse(localStorage.getItem('USER'));
+
+    // questImage.src = `../assets/${choice.render}`;
+
+    
+    // resultsSpan.textContent = choice.result;
+    // // use the selectionId to set the property dynamically //
+    // user.completed[questId] = true;
+    
+    
+    
+    
+    // if (choice.house) {
+    //     user.house = choice.house;
+    // }   
+    // if (choice.pet) {
+    //     user.pet = choice.pet;
+    // }   
+   
+    // if (choice.wand) {
+    //     user.wand = choice.wand;
+    // }
+    
+    // localStorage.setItem('USER', JSON.stringify(user));
+    
+    sleepSection.style.display = 'none';
+    moodSection.style.display = 'none';
+    energySection.style.display = 'flex';
+
+    
+    
+});
+
+// energy event listners //
+
+const energyButton = document.createElement('button');
+energyButton.textContent = 'Submit!';
+energyForm.appendChild(energyButton);
+
+// event listners //
+energyForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // const formData = new FormData(form);
+
+    
+    // const selectionId = formData.get('choices');
+    // const choice = findById(quest.choices, selectionId);
+    // const user = JSON.parse(localStorage.getItem('USER'));
+
+    // questImage.src = `../assets/${choice.render}`;
+
+    
+    // resultsSpan.textContent = choice.result;
+    // // use the selectionId to set the property dynamically //
+    // user.completed[questId] = true;
+    
+    
+    
+    
+    // if (choice.house) {
+    //     user.house = choice.house;
+    // }   
+    // if (choice.pet) {
+    //     user.pet = choice.pet;
+    // }   
+   
+    // if (choice.wand) {
+    //     user.wand = choice.wand;
+    // }
+    
+    // localStorage.setItem('USER', JSON.stringify(user));
+    
+    sleepSection.style.display = 'none';
+    moodSection.style.display = 'none';
+    energySection.style.display = 'none';
+    mbsSection.style.display = 'flex';
+
+    
+    
+});
+
+// energy event listners //
+
+const mbsButton = document.createElement('button');
+mbsButton.textContent = 'Submit!';
+mbsForm.appendChild(mbsButton);
+
+// event listners //
+mbsForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // const formData = new FormData(form);
+
+    
+    // const selectionId = formData.get('choices');
+    // const choice = findById(quest.choices, selectionId);
+    // const user = JSON.parse(localStorage.getItem('USER'));
+
+    // questImage.src = `../assets/${choice.render}`;
+
+    
+    // resultsSpan.textContent = choice.result;
+    // // use the selectionId to set the property dynamically //
+    // user.completed[questId] = true;
+    
+    
+    
+    
+    // if (choice.house) {
+    //     user.house = choice.house;
+    // }   
+    // if (choice.pet) {
+    //     user.pet = choice.pet;
+    // }   
+   
+    // if (choice.wand) {
+    //     user.wand = choice.wand;
+    // }
+    
+    // localStorage.setItem('USER', JSON.stringify(user));
+    
+    sleepSection.style.display = 'none';
+    moodSection.style.display = 'none';
+    energySection.style.display = 'none';
+    mbsSection.style.display = 'flex';
+
+    // window.location = '../results.html';
+    
+
+    
+    
+});
