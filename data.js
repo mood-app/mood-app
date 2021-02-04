a13046b2ee80d761052f0588b5efce1d4a944500
Kat Sauma <katsauma@gmail.com>
2021-02-03 17:54:44 -0800

    high energy video meg thee stallion

--- data.js
+++ data.js
@@ -61,7 +61,7 @@ export const energy = {
         high: {
             id: 'high',
             description: 'High',
-            result: ['https://www.youtube.com/embed/nVfD906XCLY, https://www.youtube.com/embed/7PBYGu4Az8s', 'https://www.youtube.com/embed/fGxVtubLjB4, https://www.youtube.com/embed/7PBYGu4Az8s', 'https://www.youtube.com/embed/8DZktowZo_k, https://www.youtube.com/embed/7PBYGu4Az8s'],
+            result: ['https://www.youtube.com/embed/nVfD906XCLY', 'https://www.youtube.com/embed/fGxVtubLjB4', 'https://www.youtube.com/embed/8DZktowZo_k'],
         }
     }
 };
